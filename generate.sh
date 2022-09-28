af0fffac0d56a1b4b4f5044af3759284b7fe3956
Andreas Göbel <andreas.goebel@eliona.io>
2022-09-28 09:13:06 +0200

    new dashboard stuff v2

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@ docker run --rm \
     openapitools/openapi-generator-cli:v6.2.0 generate \
     -g go \
     --git-user-id eliona-smart-building-assistant \
-    --git-repo-id python-eliona-api-client \
+    --git-repo-id python-eliona-api-client/v2 \
     -i https://raw.githubusercontent.com/eliona-smart-building-assistant/eliona-api/develop/openapi.yaml \
     -o /local \
     --additional-properties="packageName=api,packageVersion=1.0.0"
