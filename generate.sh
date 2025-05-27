5c2fa2678c87e707b57e5233fc7da9f58ce4de3b
Zdeněk Devátý <zdenek.devaty@gmail.com>
2025-05-27 13:58:39 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ docker run --rm \
     -g go \
     --git-user-id eliona-smart-building-assistant \
     --git-repo-id go-eliona-api-client/v2 \
-    -i https://raw.githubusercontent.com/eliona-smart-building-assistant/eliona-api/main/openapi.yaml \
+    -i https://raw.githubusercontent.com/zdevaty/eliona-api/main/openapi.yaml \
     -o /local \
     --additional-properties="packageName=api"
 
